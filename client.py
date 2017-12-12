4511ce9cef2abb7569de3242df58dca6f2d48c4c
yhao <hyppenny7@gmail.com>
2017-12-12 23:00:54 +0000

    Client view file from server

--- client.py
+++ client.py
@@ -1,5 +1,11 @@
 import json, requests
 
-request = requests.get("http://127.0.0.1:2333/test")
-print(json.loads(request.text))
-request = requests.post("http://127.0.0.1:2333/test", json = {'post': "Connected"})
+if __name__ == "__main__":
+    option = int (input("Please input a number from menu:\n1.View file\n2.Read file\n3.Edit file\n4.Add file\n5.Delete file\n"))
+    if option == 1:
+        request = requests.get("http://127.0.0.1:2333/test")
+        filelist = json.loads(request.text)
+    for f in filelist:
+        print(f)
+
+request = requests.post("http://127.0.0.1:2333/test", json={'post': "Connected"})
--- server.py
+++ server.py
@@ -1,21 +1,30 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
-
+import os
 
 app = Flask(__name__)
 api = Api(app)
 
 
-class test(Resource):
+class Test(Resource):
     def get(self):
-        return "Connecting"
+        #return "Connecting"
+        files = []
+        file_path = os.path.dirname(os.path.realpath(__file__)) + "/example"
+        print(file_path)
+
+        for filename in os.listdir(file_path):
+            print(filename)
+            files.append(filename)
+        print(files)
+        return files
 
     def post(self):
         request = reqparse.RequestParser()
         request.add_argument('post', type = str, location = 'json')
         print(request.parse_args()['post'])
 
-api.add_resource(test, '/test')
+api.add_resource(Test, '/test')
 
 if __name__ == '__main__':
     app.run(port = 2333)
