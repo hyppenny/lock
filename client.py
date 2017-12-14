d9a2f0b3080564815dcb859bd1c2293fa1921c21
yhao <hyppenny7@gmail.com>
2017-12-14 20:11:03 +0000

    update menu

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ from clientLibrary import clientLibrary
 
 if __name__ == "__main__":
     while True:
-        option = int (input("\nPlease input a number from menu:\n1.View file\n2.Read file\n3.Edit file\n4.Add file\n5.Delete file\n6.Exit\n"))
+        option = int (input("\nPlease input a number from menu:\n1.View file\n2.Read file\n3.Edit file\n4.Add file\n5.Delete file\n6.View folder\n\n7.Add folder\n8.Rename folder\n9.Delete folder\n10.Exit"))
         if option == 1:
             clientLibrary.filelist(clientLibrary)
 
@@ -29,4 +29,15 @@ if __name__ == "__main__":
 
         elif option == 6:
             break
-#request = requests.post("http://127.0.0.1:2333/test", json={'post': "Connected"})
+
+        elif option == 7:
+            break
+
+        elif option == 8:
+            break
+
+        elif option == 9:
+            break
+
+        elif option == 10:
+            break
